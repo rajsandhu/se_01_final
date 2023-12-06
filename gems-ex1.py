f28bfd33836f788d4fca798326c34054d54e0051
Raj Sandhu <rajsandhu@gmail.com>
2023-12-06 14:50:31 +0100

    first commit with a hello world statement

--- gems-ex1.py
+++ gems-ex1.py
@@ -6,6 +6,8 @@ Raj Sandhu
 """
 
 def main():
-    # (1) make a 3x3 grid of numbers
-    # (2) swap the contents of two cells
-    # (3) test whether any rows or columns now contain indentical numbers 
+
+    print("hello world")
+
+if __name__ == "__main__":
+    main()
