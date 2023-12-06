f1008d6335d7582b969d901764880becca45c840
Raj Sandhu <rajsandhu@gmail.com>
2023-12-06 14:50:19 +0100

    first commit with a hello world statement

---
+++ gems-ex1.py
@@ -0,0 +1,11 @@
+""" 
+the first exercise for the final exam 06.12.2023
+Dojo-Style SE_01 Assessment: 6 Dec 2023
+gems-ex.1pdf
+Raj Sandhu
+"""
+
+def main():
+    # (1) make a 3x3 grid of numbers
+    # (2) swap the contents of two cells
+    # (3) test whether any rows or columns now contain indentical numbers 
